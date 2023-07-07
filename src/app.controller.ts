27e8f891f77b8bd389558637b3b18ac58a3cea40
arraysArrais <joaopedroarrais@gmail.com>
2023-07-07 01:49:52 -0300

    :recycle: refactor: app controller & mail service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,18 +6,24 @@ export class AppController {
   constructor(
     private readonly mailService: MailService,
 
-    ) { }
+  ) { }
 
   @Get()
   async mailProcess() {
-    let token = await this.mailService.getTokenFromAppServer();
-    let lettersToSend = await this.mailService.getLetterstoSend();
+    try {
+      let token = await this.mailService.getTokenFromAppServer();
+      let lettersToSend = await this.mailService.getLetterstoSend();
 
-    lettersToSend.forEach(async (e) => {
-      await this.mailService.sendMail(e.recipient_email, e.title, e.content)
-    })
+      lettersToSend.forEach(async (e) => {
+        await this.mailService.sendMail(e.recipient_email, e.title, e.content)
+      })
 
-    console.log(token);
-    console.log(lettersToSend);
+      console.log(token);
+      console.log(lettersToSend);
+    }
+    catch (error) {
+      console.error(error);
+    }
+
   }
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -5,67 +5,53 @@ const nodemailer = require("nodemailer");
 
 @Injectable()
 export class MailService {
-  constructor(private readonly httpService: HttpService) { }
+    constructor(private readonly httpService: HttpService) { }
 
-  async getTokenFromAppServer() {
-    try {
-      const url = 'http://localhost:8000/api/auth/login';
+    async getTokenFromAppServer() {
+        const url = 'http://localhost:8000/api/auth/login';
 
-      const requestBody = new URLSearchParams({
-        email: process.env.SERVER_USER,
-        password: process.env.SERVER_PASS,
-      });
-      const requestHeaders = { 'Content-Type': 'application/x-www-form-urlencoded' };
+        const requestBody = new URLSearchParams({
+            email: process.env.SERVER_USER,
+            password: process.env.SERVER_PASS,
+        });
+        const requestHeaders = { 'Content-Type': 'application/x-www-form-urlencoded' };
 
-      const response = await this.httpService.post(url, requestBody.toString(), {
-        headers: requestHeaders
-      }).pipe(map((res) => res.data)).toPromise();
+        const response = await this.httpService.post(url, requestBody.toString(), {
+            headers: requestHeaders
+        }).pipe(map((res) => res.data)).toPromise();
 
-      return response['access_token'];
-
-    } catch (error) {
-      console.log(error);
+        return response['access_token'];
     }
-  }
 
-  async getLetterstoSend() {
-    try {
-      const url = 'http://localhost:8000/api/lettersToSend';
-      const requestHeaders = { 'Content-Type': 'application/json' };
-      const response = await this.httpService.get(url, {
-        headers: requestHeaders
-      }).pipe(map((res) => res.data)).toPromise();
+    async getLetterstoSend() {
+        const url = 'http://localhost:8000/api/lettersToSend';
+        const requestHeaders = { 'Content-Type': 'application/json' };
+        const response = await this.httpService.get(url, {
+            headers: requestHeaders
+        }).pipe(map((res) => res.data)).toPromise();
 
-      return response;
+        return response;
+
     }
-    catch (error) {
-      console.log(error);
+
+    async sendMail(to, subject, text) {
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            service: "gmail",
+            secure: true,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            }
+        });
+
+        const info = await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to,
+            subject,
+            text,
+        });
+
+        console.log("Email enviado para " + to + " ID: ", info.messageId);
     }
-  }
-
-  async sendMail(to, subject, text) {
-    try {
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        service: "gmail",
-        secure: true,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        }
-      });
-
-      const info = await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        text,
-      });
-
-      console.log("Email enviado para "+to+" ID: ", info.messageId);
-
-    } catch (error) {
-      console.error(error);
-    }
-  }
 }
